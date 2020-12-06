6dae2fb27824bc3c0c64a1cea36e6f9e7a00d1b8
Simpleboy353 <neelgamerxyz@gmail.com>
2020-12-06 19:42:51 +0530

    Slowmode S

--- commands/slowmode.js
+++ commands/slowmode.js
@@ -8,28 +8,28 @@ module.exports = {
         if(args[0] === amount + "s") {
         message.channel.setRateLimitPerUser(amount)
         if(amount > 1) {
-        message.channel.send("slowmode is now " + amount + " seconds")
+        message.channel.send("Slowmode is now " + amount + " seconds")
         return
         }
-        else {message.channel.send("slowmode is now " + amount + " second")
+        else {message.channel.send("Slowmode is now " + amount + " second")
         return }
     } if(args[0] === amount + "min") {
         message.channel.setRateLimitPerUser(amount * 60)
         if(amount > 1) {
-        message.channel.send("slowmode is now " + amount + " minutes")
+        message.channel.send("Slowmode is now " + amount + " minutes")
         return
         } else { 
-            message.channel.send("slowmode is now " + amount + " minute")   
+            message.channel.send("Slowmode is now " + amount + " minute")   
              
     
     return }
     } if(args[0] === amount + "h") {
         message.channel.setRateLimitPerUser(amount * 60 * 60)
         if(amount > 1) {
-        message.channel.send("slowmode is now " + amount + " hours")
+        message.channel.send("Slowmode is now " + amount + " hours")
         return
         } else {
-            message.channel.send("slowmode is now " + amount + " hour")
+            message.channel.send("Slowmode is now " + amount + " hour")
         return}
     } else {
         message.channel.send("You can only set seconds(s), minutes(min) and hours(h)")
